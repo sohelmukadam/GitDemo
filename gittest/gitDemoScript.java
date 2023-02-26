6c666913de161148279aa36ed4dd1347e6dc2504
sohelmukadam <sohelrio37@gmail.com>
2023-02-26 16:50:16 +0530

    Git Script Code

---
+++ gittest/gitDemoScript.java
@@ -0,0 +1,25 @@
+package gittest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class gitDemoScript {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\drivers\\chromedriver_win32101\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://demoqa.com/alerts");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("alertButton")).click();
+        Alert myAlert = driver.switchTo().alert();
+        System.out.println(myAlert.getText());
+	System.out.println("Inside the Branch-1");
+	System.out.println("Adding print statement for new branch");
+        myAlert.accept();
+	}
+
+}
